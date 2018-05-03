c9d44d256ee47f7a016fd672486f98f122b5bf90
rafaelalfinito@hotmail.com <rafaelalfinito@hotmail.com>
2018-05-03 22:42:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay(arrMusicians, arrInstruments){
   
   var instrument = arrInstruments[0]
   for(var i = 0; i < arrMusicians.length; i++){
-    arr.push(arrMusicians[i] + instrument)
+    arr.push(arrMusicians[i] + arrInstruments[0])
     console.log(arr)
   }
   return arr
