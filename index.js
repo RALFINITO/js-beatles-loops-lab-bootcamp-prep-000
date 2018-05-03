fc86a3f4646a4a5d6bf32bec4c1b44c33c3812d8
rafaelalfinito@hotmail.com <rafaelalfinito@hotmail.com>
2018-05-03 22:43:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ function theBeatlesPlay(arrMusicians, arrInstruments){
   
   for(var i = 0; i < arrMusicians.length; i++){
     arr.push(arrMusicians[i] + " plays " + arrInstruments[i])
-    console.log(arr)
   }
   return arr
+}
+
+
+function johnLennonFacts(arr){
+  for(var i = 0; i < arrMusicians.length; i++){
+    
+  }
 }
